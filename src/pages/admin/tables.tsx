6c5e62098776487641fbd8a486b1c97b4ae444e1
Albin <alvinfer67@gmail.com>
2024-10-19 00:24:54 -0600

    add: feature add new table

--- src/pages/admin/tables.tsx
+++ src/pages/admin/tables.tsx
@@ -1,8 +1,8 @@
 import { gql, useMutation } from '@apollo/client'
 import { useQuery } from '@apollo/client'
-import { Edit2, Table, Trash2 } from 'lucide-react'
+import { Edit2, Table, Trash2, PlusCircle } from 'lucide-react'
 import { Button } from '../../components/ui/button'
-import {  useState } from 'react'
+import { useState } from 'react'
 import {
     Dialog,
     DialogContent,
@@ -38,6 +38,13 @@ const GET_TABLES = gql`
 }
 
 `
+const ADD_TABLE = gql`
+    mutation ADD_TABLE($tableInput: TableInput) {
+        addTable(tableInput: $tableInput) {
+         name
+    }
+}
+`
 
 const UPDATE_TABLE = gql`
     mutation UPDATE_TABLE($tableUpdate: TableUpdate) {
@@ -51,9 +58,14 @@ const UPDATE_TABLE = gql`
 export default function AdminTables() {
     const { data } = useQuery<Data>(GET_TABLES);
     const [editingTable, setEditingTable] = useState<Table | null>(null);
-    const [ addTableMutation ] = useMutation<{ updateTable: Table}>(UPDATE_TABLE, { refetchQueries:[ { query: GET_TABLES}] });
+    const [updateTableMutation] = useMutation(UPDATE_TABLE, { refetchQueries: [{ query: GET_TABLES }] });
+    const [addTableMutation] = useMutation(ADD_TABLE, { refetchQueries: [{ query: GET_TABLES }] });
     const [isDialogOpen, setIsDialogOpen] = useState(false)
-
+    const [isAddDialogOpen, setIsAddDialogOpen] = useState(false);
+    const [newTable, setNewTable] = useState({
+        name: '',
+        seats: 0
+      });
     const handleEdit = (table: Table) => {
         setEditingTable(table);
         setIsDialogOpen(true)
@@ -62,15 +74,15 @@ export default function AdminTables() {
     const handleSave = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
         if (editingTable) {
-           addTableMutation({ variables: { tableUpdate: { id:editingTable.id, name: editingTable.name, seats: Number(editingTable.seats)}}});
-           setIsDialogOpen(false);
-           toast("Informacion de la mesa actualizada", {
-            description: `Ahora el nombre de la mesa es: ${ editingTable.name} y asientos: ${editingTable.seats} `,
-            action: {
-              label: "Aceptar",
-              onClick: () => {},
-            },
-          })
+            updateTableMutation({ variables: { tableUpdate: { id: editingTable.id, name: editingTable.name, seats: Number(editingTable.seats) } } });
+            setIsDialogOpen(false);
+            toast("Informacion de la mesa actualizada", {
+                description: `Ahora el nombre de la mesa es: ${editingTable.name} y asientos: ${editingTable.seats} `,
+                action: {
+                    label: "Aceptar",
+                    onClick: () => { },
+                },
+            })
         }
     }
 
@@ -82,9 +94,66 @@ export default function AdminTables() {
             })
         }
     }
+
+    const handleNewTableInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNewTable({
+         ...newTable,
+         [event.target.name]: event.target.value
+        })
+      }
+    
+      const handleAddTable = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        addTableMutation({ variables: {
+            tableInput: { name: newTable.name, seats: Number(newTable.seats)}
+        }})
+        setIsAddDialogOpen(false);
+        setNewTable({ name: '', seats: 0})
+      }
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-2xl font-bold mb-6 text-center">Mesas del Restaurante</h1>
+            <div className="mb-4 flex justify-end">
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button className="flex items-center" onClick={()=> setIsAddDialogOpen(true)}>
+                            <PlusCircle className="h-4 w-4 mr-2" />
+                            Agregar Mesa
+                        </Button>
+                    </DialogTrigger>
+                    { isAddDialogOpen && (
+                        <DialogContent className="sm:max-w-[425px]">
+                        <DialogHeader>
+                            <DialogTitle>Agregar Nueva Mesa</DialogTitle>
+                        </DialogHeader>
+                        <form onSubmit={handleAddTable} className="space-y-4">
+                            <div className="space-y-2">
+                                <Label htmlFor="new-name">Nombre de la mesa</Label>
+                                <Input
+                                    id="new-name"
+                                    name="name"
+                                    value={newTable.name}
+                                    onChange={handleNewTableInputChange}
+                                    required
+                                />
+                            </div>
+                            <div className="space-y-2">
+                                <Label htmlFor="new-seats">Cantidad de asientos</Label>
+                                <Input
+                                    id="new-seats"
+                                    name="seats"
+                                    type="number"
+                                    value={newTable.seats}
+                                    onChange={handleNewTableInputChange}
+                                    required
+                                />
+                            </div>
+                            <Button type="submit">Agregar Mesa</Button>
+                        </form>
+                    </DialogContent>
+                    )}
+                </Dialog>
+            </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {data?.tables?.map((table) => (
                     <div key={table.id} className="bg-white rounded-lg shadow-md overflow-hidden">
@@ -109,42 +178,42 @@ export default function AdminTables() {
                                     {
                                         isDialogOpen && (
                                             <DialogContent className="sm:max-w-[425px]">
-                                        <DialogHeader>
-                                            <DialogTitle>Editar Mesa {table.table_number}</DialogTitle>
-                                        </DialogHeader>
-                                        <form onSubmit={handleSave} className="space-y-4">
-                                            <div className="space-y-2">
-                                                <Label htmlFor="name">Nombre de la mesa</Label>
-                                                <Input
-                                                    id="name"
-                                                    name="name"
-                                                    value={editingTable?.name || ""}
-                                                    onChange={handleInputChange}
-                                                />
-                                            </div>
-                                            <div className="space-y-2">
-                                                <Label htmlFor="number">Número de mesa</Label>
-                                                <Input
-                                                    id="number"
-                                                    name="number"
-                                                    type="number"
-                                                    value={editingTable?.table_number || ""}
-                                                    disabled
-                                                />
-                                            </div>
-                                            <div className="space-y-2">
-                                                <Label htmlFor="seats">Cantidad de asientos</Label>
-                                                <Input
-                                                    id="seats"
-                                                    name="seats"
-                                                    type="number"
-                                                    value={editingTable?.seats || ""}
-                                                    onChange={handleInputChange}
-                                                />
-                                            </div>
-                                            <Button type="submit">Guardar cambios</Button>
-                                        </form>
-                                    </DialogContent>
+                                                <DialogHeader>
+                                                    <DialogTitle>Editar Mesa {table.table_number}</DialogTitle>
+                                                </DialogHeader>
+                                                <form onSubmit={handleSave} className="space-y-4">
+                                                    <div className="space-y-2">
+                                                        <Label htmlFor="name">Nombre de la mesa</Label>
+                                                        <Input
+                                                            id="name"
+                                                            name="name"
+                                                            value={editingTable?.name || ""}
+                                                            onChange={handleInputChange}
+                                                        />
+                                                    </div>
+                                                    <div className="space-y-2">
+                                                        <Label htmlFor="number">Número de mesa</Label>
+                                                        <Input
+                                                            id="number"
+                                                            name="number"
+                                                            type="number"
+                                                            value={editingTable?.table_number || ""}
+                                                            disabled
+                                                        />
+                                                    </div>
+                                                    <div className="space-y-2">
+                                                        <Label htmlFor="seats">Cantidad de asientos</Label>
+                                                        <Input
+                                                            id="seats"
+                                                            name="seats"
+                                                            type="number"
+                                                            value={editingTable?.seats || ""}
+                                                            onChange={handleInputChange}
+                                                        />
+                                                    </div>
+                                                    <Button type="submit">Guardar cambios</Button>
+                                                </form>
+                                            </DialogContent>
                                         )
                                     }
                                 </Dialog>
