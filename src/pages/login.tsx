8b8add9054c6e575f3ff1e77689c5bfc32017fd3
Albin <alvinfer67@gmail.com>
2024-10-23 22:37:36 -0600

    add: feature admin login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,5 @@
 import { gql, useMutation } from "@apollo/client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 const LOGIN = gql`
  mutation LOGIN($email: String!, $password: String!) {
@@ -11,18 +11,21 @@ const LOGIN = gql`
 `;
 export default function Login() {
     const [login, { data, loading, error }] = useMutation(LOGIN);
+    const [ email, setEmail ] = useState('');
+    const [ badCredentials, setBadCredentials ] = useState(false);
+    const [ password, setPassword ] = useState('');
+  
     const navigate = useNavigate();
-    useEffect(()=>{
-        if(data?.login)
-            navigate("/register");
-    }, [data])
+    
     return (
         <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
             <div className="bg-white p-8 rounded-xl shadow-2xl w-96 transform transition-all hover:scale-105 duration-300">
                 <h2 className="text-3xl font-bold text-center text-gray-800 mb-8">Iniciar Sesión</h2>
-                <form className="space-y-6" onSubmit={(e)=> {
+                <form className="space-y-6" onSubmit={async (e)=> {
                     e.preventDefault();
-                    login({ variables: { email: "albshdksd67@gmail.com", password: "1234"}})
+                    const res = await login({ variables: { email, password}});
+                    if(res.data.login)navigate('/Admin/Dashboard');
+                    setBadCredentials(true)
                 }}>
                     <div>
                         <label htmlFor="email" className="text-sm font-medium text-gray-700 block mb-2">
@@ -32,6 +35,8 @@ export default function Login() {
                             type="email"
                             id="email"
                             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
+                            value={email}
+                            onChange={(e)=> setEmail(e.target.value)}
                             placeholder="tu@ejemplo.com"
                             required
                         />
@@ -43,6 +48,8 @@ export default function Login() {
                         <input
                             type="password"
                             id="password"
+                            value={password}
+                            onChange={(e)=> setPassword(e.target.value)}
                             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
                             placeholder="********"
                             required
@@ -56,6 +63,10 @@ export default function Login() {
                             Iniciar Sesión
                         </button>
                     </div>
+                    { badCredentials && (
+                        <p className=" text-red-600">Credenciales Invalidas</p>
+                    )}
+                    
                 </form>
                 <div className="text-center mt-6">
                     <p className="text-sm text-gray-600">
